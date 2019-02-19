366dc1d896eafec1410a02b258cfcbe1ad8cddd2
Liight <Liight@users.noreply.github.com>
2019-02-20 08:39:28 +1300

    Save image picked to local state

--- App.js
+++ App.js
@@ -7,8 +7,10 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+
+import ImagePicker from 'react-native-image-picker';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,13 +19,40 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+
+
 export default class App extends Component {
+
+  state = {
+    pickedImage: null
+  }
+
+
+  pickImageHandler = () => {
+    ImagePicker.showImagePicker({
+      // Same code as in above section!
+      title: "Pick an image"
+    },
+      res => {
+        if (res.didCancel) {
+          console.log("User cancelled")
+        } else if (res.error) {
+        } else {
+          this.setState({
+            pickedImage: { uri: res.uri, base64: res.data }
+          });
+          console.log(this.state.pickedImage)
+        }
+      });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to Language-Link!</Text>
         <Text style={styles.instructions}>To get started, take a photo of something in your environment.</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Button title="Pick Image" onPress={this.pickImageHandler} />
       </View>
     );
   }
